a8c15ede73b6d1da04d69643cb0f391e38d30f9a
Eric Bowerman <eric.bowerman@windstream.com>
2017-07-26 16:11:44 -0400

    Setup start script and heroky port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -20,9 +21,11 @@ app.use((request, response, next) => {
   next();
 });
 
+/*
 app.use((request, response, next) => {
   response.render('maintenance.hbs');
 });
+*/
 
 app.use(express.static(__dirname + '/public'));
 
@@ -54,6 +57,6 @@ app.get('/bad', (request, response) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000.');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}.`);
 });
